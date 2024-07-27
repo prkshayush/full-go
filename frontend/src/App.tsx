b0186b45988c532716eea7b1d376a74e58e77502
Ayush Prakash <92213261+prkshayush@users.noreply.github.com>
2024-07-27 23:59:48 +0530

    more ui changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { Navbar } from './components/Navbar'
 import { TodoForm } from './components/TodoForm'
 import Todos from './components/Todos'
 
@@ -7,7 +6,6 @@ function App() {
 
   return (
     <div className="h-[100vh]">
-      <Navbar />
       <h2 className="text-3xl font-bold text-center ">React + Go todo application</h2>
       <div className="flex items-center justify-between gap-10 p-8">
         <Todos />
